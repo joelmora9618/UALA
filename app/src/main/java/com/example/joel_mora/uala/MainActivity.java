65e6e5ed3b698cc1fcff445d7a96e048677c354c
ITRESOURCES\Joel_Mora <joel_mora@itrsa.com.ar>
2018-10-03 09:53:23 -0300

    app initial

---
+++ app/src/main/java/com/example/joel_mora/uala/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.joel_mora.uala;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
